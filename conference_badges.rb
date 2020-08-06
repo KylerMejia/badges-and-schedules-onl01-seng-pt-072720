eb0a7c9b3800e94b90355598ca6f1f11eefb3e9c
Kyler Mejia <kylermejia1@gmail.com>
2020-08-06 21:26:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,12 @@
-def badge_maker(speaker)
- return "Hello, my name is #{speaker}."
+def assign_rooms(speakers_lineup)
+ speakers_lineup.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
 end
 
-speakers_lineup = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def batch_badge_creator(speakers_lineup)
- speakers_lineup.collect {|speaker| badge_maker(speaker)}
+def printer(speakers)
+ batch_badge_creator(speakers).each do |badge|
+  puts badge
+ end
+ assign_rooms(speakers).each do |assignment|
+  puts assignment
+ end
 end
